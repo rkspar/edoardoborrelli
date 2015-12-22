40f3f64da121229a021c5594918c521952e2b11e
Randall Escalona <re@deepbluedev.co>
2015-12-22 10:23:50 -0500

    final stage

--- assets/js/vitality.js
+++ assets/js/vitality.js
@@ -86,8 +86,8 @@
     // Formstone Background - Video Background Settings
     $("header.video").background({
         source: {
-            poster: "assets/img/bg-mobile-fallback.jpg",
-            mp4: "assets/mp4/camera.mp4"
+            poster: "assets/img/borrelli.jpg",
+            mp4: "assets/mp4/borrellitailor.mp4"
         }
     });
 
